b4e11c3d41b6a49e6b7b3cfac861a0417b62948c
Adam Prugel-Bennett <apb@ecs.soton.ac.uk>
2025-03-27 12:40:03 +0000

    updated graph to be more stable

--- site/labs/A3/code/graph.cc
+++ site/labs/A3/code/graph.cc
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <cstdlib>
 #include <queue>
+#include <stdio.h>
 using namespace std;
 
 /* Structure to hold posision of vertices */
@@ -57,11 +58,11 @@ struct Edge {
   Edge(int v1, int v2, double d): vertex1(v1), vertex2(v2), distance(d) {}
 };
 
-class CompareEdges {
-  bool operator()(Edge e1, Edge e2) {
-    return e1.distance < e2.distance;
-  }
-};
+
+bool operator<(const Edge& e1, const Edge& e2) {
+  return e1.distance > e2.distance;  // Ensure min-heap not max-heap
+}
+
 
 /* Graph class */
 
@@ -78,84 +79,104 @@ public:
     std::random_device rd;  
     std::mt19937 gen(rd()); 
     std::uniform_real_distribution<double> randCoordinate(-50.0, 50.0);
-    double len = 85.0/sqrt(n);
+    double len = 75.0/sqrt(n);
     // create n Vertices that are nicely spaced
-    for(unsigned i=0; i<n; ++i) {
-      Position pos;
-      bool okPosition;
-      do {
-	okPosition = true;
-	pos = Position(randCoordinate(gen), randCoordinate(gen));
-	if (pos.norm() < 15) {
-	  okPosition = false;
-	}
-	if (okPosition) {
-	  for(unsigned j=0; j<i; ++j) {
-	    if ((pos - vertices[j]).norm() < len) {
-	      okPosition = false;
-	      break;
+    bool too_many_iterations;
+    do {  // redo if necessary
+      too_many_iterations = false;
+      for(unsigned i=0; i<n; ++i) {
+	Position pos;
+	bool okPosition;
+	do {
+	  okPosition = true;
+	  pos = Position(randCoordinate(gen), randCoordinate(gen));
+	  if (pos.norm() < 15) {
+	    okPosition = false;
+	  }
+	  if (okPosition) {
+	    for(unsigned j=0; j<i; ++j) {
+	      if ((pos - vertices[j]).norm() < len) {
+		okPosition = false;
+		break;
+	      }
 	    }
 	  }
-	}
-      } while(!okPosition);
-      vertices.push_back(pos);
-    }
+	} while(!okPosition);
+	vertices.push_back(pos);
+      }
 
-    if (edgeProb>1.0) {
-      cout << "error: edgeProb must be less than 1\n";
-    }
-    if (edgeProb>0.75) {
-      cout << "warning: high edgeProb migh mean we cannot generate a pretty graph\n";
-    }
-    num_edges = (unsigned) (edgeProb*n*n/2);
-    cout << n << " " << edgeProb << " " << num_edges << endl;
+      if (edgeProb>1.0) {
+	cout << "error: edgeProb must be less than 1\n";
+      }
+      if (edgeProb>0.75) {
+	cout << "warning: high edgeProb migh mean we cannot generate a pretty graph\n";
+      }
+      num_edges = (unsigned) (edgeProb*n*n/2);
     
-    std::uniform_int_distribution<int> randVertex(0, n-1);
-    for(unsigned i=0; i<num_edges; ++i) {
-      unsigned v1, v2;
-      bool okEdge;
-      do {
-	v1 = randVertex(gen);
-	v2 = randVertex(gen);
-	okEdge = true;
-	if (v1 == v2) {
-	  okEdge = false;
-	}
-	if (okEdge) {
-	  // stop repetitions of edges
-	  for (unsigned k=0; k<adjacencyList[v1].size(); ++k) {
-	    if (adjacencyList[v1][k].vertex2 == v2) {
-	      okEdge = false;
-	      break;
-	    }
+      std::uniform_int_distribution<int> randVertex(0, n-1);
+      int cnt = 0;
+      for(unsigned i=0; i<num_edges; ++i) {
+	unsigned v1, v2;
+	bool okEdge;
+	if (too_many_iterations)
+	    break;
+	do {
+	  v1 = randVertex(gen);
+	  v2 = randVertex(gen);
+	  okEdge = true;
+	  ++cnt;
+	  if (cnt>10000) {
+	    too_many_iterations = true;
+	    okEdge = false;
+	    break;
 	  }
-	}
-	if (okEdge) {
-	  // stop edges going close to vertices
-	  for(unsigned j=0; j<n; ++j) {
-	    if (j==v1 || j==v2)
-	      continue;
-	    Position d13 = vertices[v1] - vertices[j];
-	    Position d23 = vertices[v2] - vertices[j];
-	    double n13 = dot(d13, d13);
-	    double n23 = dot(d23, d23);
-	    double d = dot(d13, d23);
-	    double t = (n13-d)/(n23+n13-2.0*d);
-	    if (t>0.0 && t<1.0) {
-	      if (((1.0-t)*d13 + t*d23).norm()< 15) {
+	  if (v1 == v2) {
+	    okEdge = false;
+	  }
+	  if (okEdge) {
+	    // stop repetitions of edges
+	    for (unsigned k=0; k<adjacencyList[v1].size(); ++k) {
+	      if (adjacencyList[v1][k].vertex2 == v2) {
 		okEdge = false;
 		break;
 	      }
 	    }
 	  }
+	  if (okEdge) {
+	    // stop edges going close to vertices
+	    for(unsigned j=0; j<n; ++j) {
+	      if (j==v1 || j==v2)
+		continue;
+	      Position d13 = vertices[v1] - vertices[j];
+	      Position d23 = vertices[v2] - vertices[j];
+	      double n13 = dot(d13, d13);
+	      double n23 = dot(d23, d23);
+	      double d = dot(d13, d23);
+	      double t = (n13-d)/(n23+n13-2.0*d);
+	      if (t>0.0 && t<1.0) {
+		if (((1.0-t)*d13 + t*d23).norm()< 15) {
+		  okEdge = false;
+		  break;
+		}
+	      }
+	    }
+	  }
+	  if (too_many_iterations)
+	    break;
+	} while(!okEdge);
+	if (too_many_iterations) {
+	  break;
 	}
-      } while(!okEdge);
-      Edge e(v1, v2, (vertices[v1] - vertices[v2]).norm());
-      adjacencyList[v1].push_back(e);
-      e.vertex1 = v2;
-      e.vertex2 = v1;
-      adjacencyList[v2].push_back(e);
-    }
+	Edge e(v1, v2, (vertices[v1] - vertices[v2]).norm());
+	adjacencyList[v1].push_back(e);
+	e.vertex1 = v2;
+	e.vertex2 = v1;
+	adjacencyList[v2].push_back(e);
+      }
+      if (too_many_iterations) {
+	break;
+      }
+    } while (too_many_iterations);
   }
 
   unsigned size() const {return num_vertices;}
@@ -189,13 +210,18 @@ public:
     draw(edges);
   }
   
-  void draw(const vector<Edge>& highlightEdges) const {
-    ofstream plot("plot_graph.py");
+  void draw(const vector<Edge>& highlightEdges, string title="") const {
+    char file_name[] = "tmpfileXXXXXX";
+    mkstemp(file_name);
+    ofstream plot(file_name);
 
     plot << "import numpy as np\n";
     plot << "import matplotlib.pyplot as plt\n";
     plot << "fig, ax = plt.subplots()\n";
     plot << "ax.set_aspect('equal', adjustable='datalim')\n";
+    if (title!="") {
+      plot << "plt.title(\"" << title << "\")\n";
+    }
 
     for(unsigned i=0; i<size(); ++i) {
       for(const Edge& e: adjacencyList[i]) {
@@ -205,7 +231,7 @@ public:
       }
     }
     for(const Edge& e: highlightEdges) {
-      plot << matplotlibEdge(e, "b", 1);
+      plot << matplotlibEdge(e, "b", 1, 2);
     }
 	  
     int cnt = 0;
@@ -218,21 +244,22 @@ public:
     plot << "plt.show()\n";
     plot.close();
     sleep(0.2);
-    system("python plot_graph.py&");
+    string cmd = "python " + string(file_name) + " &";
+    system(cmd.c_str());
   }
 
   vector<Edge> prims();
-  vector<Edge> kruskal();
+  vector<Edge> kruskals();
 
 private:
   inline
-  string matplotlibEdge(const Edge& e, string col="ro", int zorder=0) const {
+  string matplotlibEdge(const Edge& e, string col="ro", int zorder=0, double lw=1) const {
     Position v1 = vertices[e.vertex1];
     Position v2 = vertices[e.vertex2];
     stringstream ss;
     ss << "plt.plot([" << v1.x << "," << v2.x << "],["
        << v1.y << "," << v2.y << "], \""
-       << col << "-\", zorder=" << zorder << ")\n";
+       << col << "-\", zorder=" << zorder << ", linewidth=" << lw << ")\n";
     return ss.str();
   }
 };
@@ -241,14 +268,18 @@ private:
 vector<Edge> Graph::prims() {
   vector<Edge> mst;
 
-  priority_queue<Edge, vector<Edge>, CompareEdges> pq;
   // You need to fill this in
+
+  draw(mst, "Prim's Algorithm");
   return mst;
 }
 
-vector<Edge> kruskal() {
+vector<Edge> Graph::kruskals() {
   vector<Edge> mst;
+  
   // You need to fill this in
+  
+  draw(mst, "Kruskal's Algorithm");
   return mst;
 }
 
@@ -256,7 +287,11 @@ vector<Edge> kruskal() {
 int main() {
   Graph graph(15, 0.3);
   graph.print();
-  vector<Edge> my_edges;
-  my_edges.push_back(graph.edges_connecting(0)[0]);
-  graph.draw(my_edges);
+
+  // For illustrive purposes
+  graph.draw(graph.edges_connecting(0), "Edges connecting vertex 0");
+
+  graph.prims();
+  graph.kruskals();
+
 }
